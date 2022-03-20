060cd1ec28ad540f0acde5e39c60439bf6b90324
injoon2019 <ison@sfu.ca>
2022-03-20 18:34:23 +0900

    fix: 이름에 띄어쓰기 없으면 하나의 이름으로 인식 오류 수정

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -4,15 +4,18 @@ import blackjack.domain.participant.Player;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String DELIMITER = ", ";
+    private static final String DELIMITER = ",";
 
     public static List<String> requestPlayerNames() {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
-        return Arrays.asList(scanner.nextLine().split(DELIMITER));
+        return Arrays.stream(scanner.nextLine().split(DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     public static String requestDecision(Player player) {
